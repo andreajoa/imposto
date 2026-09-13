ceeee19374b12b0011bac9d6b4b5f3a07a577861
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-09-12 23:22:17 -0300

    feat: enable multi-guide commerce on business-opening page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,16 @@ import { CartProvider } from './context/CartContext'
 import TaxGuide from './pages/TaxGuide'
 import ThankYou from './pages/ThankYou'
 import BusinessOpening from './pages/BusinessOpening'
+import BusinessOpeningCommerce from './components/BusinessOpeningCommerce'
+
+function BusinessOpeningRoute() {
+  return (
+    <>
+      <BusinessOpening />
+      <BusinessOpeningCommerce />
+    </>
+  )
+}
 
 export default function App() {
   return (
@@ -10,7 +20,7 @@ export default function App() {
       <CartProvider>
         <Routes>
           <Route path="/obrigado" element={<ThankYou />} />
-          <Route path="/abertura-de-empresa-nos-eua" element={<BusinessOpening />} />
+          <Route path="/abertura-de-empresa-nos-eua" element={<BusinessOpeningRoute />} />
           <Route path="*" element={<TaxGuide />} />
         </Routes>
       </CartProvider>
