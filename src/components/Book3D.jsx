d84b1460cdd06247aaa9fd6a977dc10f70efbd00
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-09-12 22:49:58 -0300

    fix: replace blue book mockup with neutral Express Solution cover

--- src/components/Book3D.jsx
+++ src/components/Book3D.jsx
@@ -4,8 +4,8 @@ export default function Book3D() {
       <div className="book-mockup-wrapper">
         <div className="book-mockup-glow" />
         <img
-          src="/image/mockup-apostila.png"
-          alt="Mockup da capa do Guia Completo de Impostos para Imigrantes nos EUA"
+          src="/image/book-cover-cart.svg"
+          alt="Capa do Guia Completo de Impostos para Imigrantes nos EUA"
           className="book-mockup-img"
         />
         <div className="book-mockup-shadow" />
