d0109e2a9d343e0a8767cbe9496eec6db6cdd91d
andreajoa <100246160+andreajoa@users.noreply.github.com>
2026-09-12 23:06:19 -0300

    feat: switch homepage to new tax guide experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartProvider } from './context/CartContext'
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import ProofBar from './components/ProofBar'
-import Problems from './components/Problems'
-import Learn from './components/Learn'
-import Tools from './components/Tools'
-import Author from './components/Author'
-import CTA from './components/CTA'
-import Contact from './components/Contact'
-import Footer from './components/Footer'
-import CartSlideOut from './components/CartSlideOut'
+import TaxGuide from './pages/TaxGuide'
 import ThankYou from './pages/ThankYou'
 import BusinessOpening from './pages/BusinessOpening'
-
-function TaxGuideLanding() {
-  return (
-    <>
-      <Navbar />
-      <Hero />
-      <ProofBar />
-      <Problems />
-      <Learn />
-      <Tools />
-      <Author />
-      <CTA />
-      <Contact />
-      <Footer />
-      <CartSlideOut />
-    </>
-  )
-}
 
 export default function App() {
   return (
@@ -39,7 +11,7 @@ export default function App() {
         <Routes>
           <Route path="/obrigado" element={<ThankYou />} />
           <Route path="/abertura-de-empresa-nos-eua" element={<BusinessOpening />} />
-          <Route path="*" element={<TaxGuideLanding />} />
+          <Route path="*" element={<TaxGuide />} />
         </Routes>
       </CartProvider>
     </BrowserRouter>
